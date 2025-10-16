d12c56b787783ba7ab6fafbc35c1015c527fe599
SparkleNL <ajansen1608@gmail.com>
2025-10-16 15:14:52 +0200

    UC19 commit 1 - Products database
    Products are retrieved from database

--- Grocery.Core.Data/Repositories/ProductRepository.cs
+++ Grocery.Core.Data/Repositories/ProductRepository.cs
@@ -1,27 +1,71 @@
 ﻿using Grocery.Core.Interfaces.Repositories;
 using Grocery.Core.Models;
+using Microsoft.Data.Sqlite;
+
 
 namespace Grocery.Core.Data.Repositories
 {
-    public class ProductRepository : IProductRepository
+    public class ProductRepository : DatabaseConnection, IProductRepository
     {
-        private readonly List<Product> products;
+        private readonly List<Product> products = [];
         public ProductRepository()
         {
-            products = [
-                new Product(1, "Melk", 300, new DateOnly(2025, 9, 25), 0.95m),
-                new Product(2, "Kaas", 100, new DateOnly(2025, 9, 30), 7.98m),
-                new Product(3, "Brood", 400, new DateOnly(2025, 9, 12), 2.19m),
-                new Product(4, "Cornflakes", 0, new DateOnly(2025, 12, 31), 1.48m)];
+            CreateTable(@"CREATE TABLE IF NOT EXISTS Product (
+                            [Id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                            [Name] TEXT NOT NULL,
+                            [Stock] INTEGER NOT NULL,
+                            [Date] DATE NOT NULL,
+                            [Price] FLOAT NOT NULL)");
+            List<string> insertQueries = [@"INSERT OR IGNORE INTO Product(Id, Name, Stock, Date, Price) VALUES(1, 'Melk', 300, '2025-09-25', 0.95)",
+                                          @"INSERT OR IGNORE INTO Product(Id, Name, Stock, Date, Price) VALUES(2, 'Kaas', 100, '2025-09-30', 7.98)",
+                                          @"INSERT OR IGNORE INTO Product(Id, Name, Stock, Date, Price) VALUES(3, 'Brood', 400, '2025-09-12', 2.19)",
+                                          @"INSERT OR IGNORE INTO Product(Id, Name, Stock, Date, Price) VALUES(4, 'Cornflakes', 0, '2025-12-31', 1.48)"];
+            InsertMultipleWithTransaction(insertQueries);
+            GetAll();
         }
         public List<Product> GetAll()
         {
+            products.Clear();
+            string selectQuery = "SELECT Id, Name, Stock, date(Date), Price FROM Product";
+            OpenConnection();
+            using (SqliteCommand command = new(selectQuery, Connection))
+            {
+                SqliteDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32(0);
+                    string name = reader.GetString(1);
+                    int stock = reader.GetInt32(2);
+                    DateOnly shelfLife = DateOnly.FromDateTime(reader.GetDateTime(3));
+                    Decimal price = reader.GetDecimal(4);
+                    products.Add(new(id, name, stock, shelfLife, price));
+                }
+            }
+            CloseConnection();
             return products;
         }
 
         public Product? Get(int id)
         {
-            return products.FirstOrDefault(p => p.Id == id);
+            string selectQuery = $"SELECT Id, Name, Stock, date(Date), Price FROM Product WHERE Id = {id}";
+            Product? p = null;
+            OpenConnection();
+            using (SqliteCommand command = new(selectQuery, Connection))
+            {
+                SqliteDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    int productId = reader.GetInt32(0);
+                    string name = reader.GetString(1);
+                    int stock = reader.GetInt32(2);
+                    DateOnly shelfLife = DateOnly.FromDateTime(reader.GetDateTime(3));
+                    Decimal price = reader.GetDecimal(4);
+                    p = new(productId, name, stock, shelfLife, price);
+                }
+            }
+            CloseConnection();
+            return p;
         }
 
         public Product Add(Product item)
