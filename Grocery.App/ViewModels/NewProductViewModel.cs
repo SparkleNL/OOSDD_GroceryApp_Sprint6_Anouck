0e51b4d3863ce0c492de0dd7d01602a60ea3f5e5
SparkleNL <ajansen1608@gmail.com>
2025-10-19 13:58:31 +0200

    UC19 commit 2 - Added files
    Added the view and viewmodel files for adding a new product.

---
+++ Grocery.App/ViewModels/NewProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Grocery.Core.Interfaces.Services;
+using Grocery.Core.Models;
+using System.Collections.ObjectModel;
+
+namespace Grocery.App.ViewModels
+{
+    public class NewProductViewModel : BaseViewModel
+    {
+    }
+}
